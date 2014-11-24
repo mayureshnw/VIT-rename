dd96b2304398e4b4eb50d06c10eba4d409960258
Mnw2212 <mayuresh2212@gmail.com>
2014-11-24 17:07:24 +0530

    added support for WINSEM files and textbook
    ex :
    Actual  : FALLSEM2014-15_CP1348_TB03_Updates-for-CAT2.docx
    Altered : TB03_Updates-for-CAT2.docx

--- rename.py
+++ rename.py
@@ -3,15 +3,26 @@ import os
 monthDict={'Jan':'1','Feb':'2','Mar':'3','Apr':'4','May':'5','Jun':'6','Jul':'7','Aug':'8','Sep':'9','Oct':'10','Nov':'11','Dec':'12'}
 
 def filechange(filename):
-	name = filename
 	global monthDict
-	day = name[22:24]
-	month = name[25:28]
-	year = name[31:33]
-	date = year+'-'+monthDict[month]+'-'+day
-	sub = name[39:]
-	newName=date+'_'+sub
-	return newName
+
+	if filename[0:3]=="FAL":
+		name=filename[1:]
+	elif filename[0:3]=="WIN":
+		name=filename
+
+	day = name[21:23]
+	try:
+		int(day)
+		month = name[24:27]
+		year = name[30:32]
+		date = year+'-'+monthDict[month]+'-'+day
+		sub = name[38:]
+		newName=date+'_'+sub
+		return newName
+	except:
+		newName = name[21:]
+		return newName
+
 
 fileNew=[]
 fileOld=[]
@@ -26,7 +37,7 @@ for dirname, dirnames, filenames in os.walk('.'):
 	    		fileNew.append(newName)
 	    		fileOld.append(filename)
 	    	except:
-	    		print " FileFormat Error : "+ filename
+	    		print " FileName Error : "+ filename
 
 
 for i in range(len(fileOld)):
